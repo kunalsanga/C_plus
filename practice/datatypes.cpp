308acf0e4709dfe4ea2f3318579197b64344e739
kunal sanga <152892694+kunalsanga@users.noreply.github.com>
2025-02-06 20:44:02 +0530

    Add files via upload

---
+++ practice/datatypes.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string name="ben tenyson";
+    int age =18;
+    cout << "name: "<<name<<endl;
+    cout << "age: " <<age<<endl;
+    return 0;
+}
