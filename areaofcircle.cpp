4deff05ca048da49ccf234c11bf006a55002b35d
kunal sanga <152892694+kunalsanga@users.noreply.github.com>
2025-02-07 23:16:33 +0530

    Add files via upload

---
+++ areaofcircle.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    int radius = 5;
+    double area = 3.14*pow(radius,2);
+    cout<<"the area of circle is: "<<area<<endl;
+    return 0;
+}
