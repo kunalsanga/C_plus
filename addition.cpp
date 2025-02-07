fcc26f56dfd4d5ca9ed267991e6bea893ce7c808
kunal sanga <152892694+kunalsanga@users.noreply.github.com>
2025-02-07 23:15:27 +0530

    Add files via upload

---
+++ addition.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 10;
+    int b = 18;
+    int sum=a+b;
+    cout<<"the sum of "<<a<< " and " <<b<< " is: "<<sum<<endl; 
+    return 0;
+
+
+}
