326dfc40ecb3192cd4bc258c50b156434416e99f
kunal sanga <152892694+kunalsanga@users.noreply.github.com>
2025-02-25 23:36:08 +0530

    Add files via upload

---
+++ datatypes_var_operators.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int age = 18;
+    float height = 187.98;
+    double pi = 3.14159;
+    char grade ='0';
+    string name ="Alice";
+    bool isstudent = true;
+
+    cout << "Age: " <<age<<endl;
+    cout << "Height: " <<height<<endl;
+    cout << "pi: " << pi<<endl;
+    cout << "Grade: " <<grade<<endl;
+    cout << "Name: " << name <<endl;
+    cout << "Is Student: " <<isstudent<<endl;
+
+    return 0;
+
+}
